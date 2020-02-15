30b7c4908497f8244017c10acbd1b5867aa283d8
yukio <3204704@gmail.com>
2020-02-15 19:01:52 +0900

    Create TableTesting.test.ts

---
+++ TableTesting.test.ts
@@ -0,0 +1,17 @@
+describe("ページネーションの表示をサポートする getPageNums 関数", () => {
+  describe.each([
+    [{ pageNums: 3, total: 8, windowSize: 5 }, [1, 2, 3, 4, 5]],
+    [{ pageNums: 4, total: 8, windowSize: 5 }, [2, 3, 4, 5, 6]],
+    [{ pageNums: 3, total: 5, windowSize: 5 }, [1, 2, 3, 4, 5]],
+  ])(
+    "現在ページ, 総ページ数, ウィンドウ幅 %o, の場合のページネーション",
+    (input, expected) => {
+      test(`${input.pageNums} ページ目は ${expected} になる`, () => {
+        const result = getPageNums(
+          ...(Object.values(input) as [number, number, number])
+        );
+        assert.deepStrictEqual(result, expected);
+      });
+    }
+  );
+});
